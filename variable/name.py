469d04df8a9f81e7b23e152f9a8ed2ce56275110
pinyo1999 <pinyo.sum@ku.th>
2021-01-10 02:33:00 +0700

    การประกาศตัวแปร

---
+++ variable/name.py
@@ -0,0 +1,14 @@
+#การตั้งชื่อ  in python 
+myvar = "pinyo"
+my_var = "pinyo"
+_my_var = "pinyo"
+myVar = "pinyo"
+MYVAR ="pinyo"
+myvar1 = "pinyo"
+# ตัวแปรที่มีการตั้งเป็นตัวอักษรพิมพ์ใหญ่ทั้งหมด นิยามไว้ว่าจะให้เก็บค่าคงที่ เช่น PI = 3.14
+
+
+#ไม่สามารถตั้งได้
+2myvar = "pinyo"
+my-var = "pinyo"
+my var = "pinyo"
