b0ab9fede2fd77255de38c0c8008170b6adcbef8
pinyo1999 <pinyo.sum@ku.th>
2021-01-16 14:54:09 +0700

    ต่อ string ใช้ method = format,f-strings

---
+++ string/f_strings.py
@@ -0,0 +1,7 @@
+name = "Petch"
+age = 21
+
+result = f" My name is {name}, and I am {age} years old."
+
+
+print("result, result")  # output : "My name is Petch, and I am 21"
---
+++ string/format.py
@@ -0,0 +1,6 @@
+age = 21
+txt = "My name is Petch, and I am {}"
+
+result = txt.format(age)
+
+ print("result", result) #output : "My name is Petch, and I am 21"
