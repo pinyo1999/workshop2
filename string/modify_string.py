d0964c9c23520080130951552a9f3e2d1720549d
pinyo1999 <pinyo.sum@ku.th>
2021-01-16 14:42:12 +0700

    method of string

---
+++ string/modify_string.py
@@ -0,0 +1,8 @@
+string = " Hello, world! "
+
+print(string.upper())  # output :" HELLO, WORLD! "
+print(string.lower())  # output :" hello, world! "
+print(string.strip())  # output : "Hello, world!"
+print(string.replace("H", "J"))  # output : jello, world!
+print(string.split(","))  # output : [' Hello', ' world! ']
+print(len(string))  # output : 15
