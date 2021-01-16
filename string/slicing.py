6171e13ff54da7728c6c7fff5855d684e434ca5a
pinyo1999 <pinyo.sum@ku.th>
2021-01-16 14:38:45 +0700

    ตัดข้อความ

---
+++ string/slicing.py
@@ -0,0 +1,8 @@
+string = "Hello, world!"
+
+print(string[2:5]) # output : llo
+print(string[:5])) # output : hello
+print(string[2:]) # output : llo, world!
+print(string[-5:-2]) # output : orl
+print(string[0]) # output : H
+print(string[-1:]) # output : !
