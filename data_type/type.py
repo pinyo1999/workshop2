3c8d1a79c32c9e7ef5174a6a38ec8ddfaa20a5e2
pinyo1999 <pinyo.sum@ku.th>
2021-01-16 14:36:16 +0700

    Type in python

---
+++ data_type/type.py
@@ -0,0 +1,10 @@
+x = "Hello World"  # str
+x = 20  # int
+x = 20.5  # float
+x = ["apple", "Banana", "Cherry"]  # list
+x = ("apple", "Banana", "Cherry")  # tuple
+x = {name: "pinyo", age: 21}  # dict
+x = {"apple", "Banana", "Cherry"}  # set
+x = frozenset({"apple", "Banana", "Cherry"})  # frozenset
+x = True  # bool
+x = b"hello"  # bytes
