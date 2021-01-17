aed7ff0bf5843cc88f9fe7ee415d0e38b4b716e9
pinyo1999 <pinyo.sum@ku.th>
2021-01-17 14:43:29 +0700

    no message

--- tuples/join_tuple.py
+++
@@ -1,6 +0,0 @@
-list1 = (1, 2, 3)
-list2 = (4, 5, 6)
-
-list3 = list1 + list2
-
-print(list3) # Output : (1, 2, 3, 4, 5, 6)
--- tuples/unpack_tuple_items.py
+++
@@ -1,7 +0,0 @@
-fruits = ("apple", "Banana", "Cherry")
-
-(green, yellow, red) = fruits
-
-print(green)
-print(yellow)
-print(red)
